ef5074aa73d335327ccff2199147c5864354f733
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-03-09 12:26:34 +0100

    Auto-sync [nixos] : 2026-03-09 12:26:34

--- Atomic_postinstall/modules/compression.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Script à utiliser pour Fedora Atomic (Silverblue / Kinoite / Ublue )"
-
-echo "Compression des fichiers existants (peut prendre un moment)..."
-sudo btrfs filesystem defragment -r -v -czstd /var &&
-sudo btrfs filesystem defragment -r -v -czstd /var/home && 
-echo "Compression des fichiers existants terminée."
--- NixOS_install_(custom-install)/nixos-dotfiles_bckp_2026-02-26/hardware-support/tunings/qemu_tunings.nix
+++
@@ -1,5 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  services.xserver.enable = true;
-}
